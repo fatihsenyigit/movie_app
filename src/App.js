a2c500c5f7a9818cf5e1361ff95889e610359932
fatih <fsenyigit8@gmail.com>
2024-05-14 22:52:07 +0300

    register page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Register from "./pages/Register";
 
 function App() {
   return (
     <div className="App">
-      
+      <Register />
     </div>
   );
 }
