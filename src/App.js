9e0bd360b67f86935c3b0d02e2943ae236c5117d
fatih <fsenyigit8@gmail.com>
2024-05-15 21:00:39 +0300

    app router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Register from "./pages/Register";
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
-    <div className="App">
-      <Register />
+    <div className="dark:bg-gray-dark-main min-h-screen">
+      <AppRouter/>
     </div>
   );
 }
